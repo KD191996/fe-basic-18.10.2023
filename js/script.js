9802474b0d3bb63b40ec891bc1e78245755e6b5d
KD191996 <katyushadanilenko82@gmail.com>
2023-12-24 22:12:24 +0200

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $('.burger').click(function () {
     $(this).toggleClass('active');
+    $('.nav-wrap').toggleClass('active');
     $('.item-3').toggleClass('active');
+
+})
+$('.nav-wrap, .nav-wrap .close').click(function (event) {
+    console.log(event.target);
+    console.log(this);
+
+    if (event.target === this) {
+        $('.nav-wrap').removeClass('active')
+    }
 
 })
